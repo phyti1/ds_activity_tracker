7f8810d6eb9ef1ac98f5f97b22497f10f4f8b54a
Simon Luder <sam.luder@bluewin.ch>
2022-06-02 15:47:13 +0200

    Add CNN_3 model class

--- Analyse/webservice/main.py
+++ Analyse/webservice/main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import os, io, json
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from math import sin, cos, sqrt, atan2, radians
 
 app = flask.Flask(__name__)
@@ -214,8 +216,42 @@ def predict_with_cnn_1(tensors, model, num_classes=7):
         if num_classes == 7:
             return itos_7[np.argmax(counts)]
 
+class CNN_3(nn.Module):
+    def __init__(self, num_classes, kernel_size=10, pool_size=3, padding=0, conv1_channels = 10, conv2_channels=8, conv3_channels=8, fc_linear_1=180, fc_linear_2=120, dropout=0):
+        '''Convolutional Net class'''
+        super(CNN_3, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=5, out_channels=conv1_channels, kernel_size=kernel_size, padding=padding) 
+        self.pool = nn.MaxPool1d(kernel_size=pool_size, stride=1)
+        self.conv2 = nn.Conv1d(in_channels=conv1_channels, out_channels=conv2_channels, kernel_size=kernel_size, padding=padding) 
+        self.conv3 = nn.Conv1d(in_channels=conv2_channels, out_channels=conv3_channels, kernel_size=kernel_size, padding=padding) 
+        self.fc1 = nn.Linear(in_features=conv3_channels*57, out_features=fc_linear_1)
+        self.fc2 = nn.Linear(in_features=fc_linear_1, out_features=fc_linear_2)
+        self.fc3 = nn.Linear(in_features=fc_linear_2, out_features=num_classes)
+        self.conv2_channels = conv2_channels
+        self.conv3_channels = conv3_channels
+        self.dropout = nn.Dropout(p=dropout)
+        
+    def forward(self, x):
+        '''
+        Applies the forward pass
+        Args:
+            x (torch.tensor): input feature tensor
+        Returns:
+            x (torch.tensor): output tensor of size num_classes
+        '''
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, self.conv3_channels*57) # flatten tensor
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
+
 if __name__ == '__main__':
-    //host on every ip avaliable
+    # host on every ip avaliable
     app.run(host="0.0.0.0", port=port)
 
 
